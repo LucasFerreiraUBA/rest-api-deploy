04f26944d7ae8d0e3d3a0f558a512d358182a779
Lucas Ferreira <lferreira@fi.uba.ar>
2025-03-23 19:09:56 -0300

    a

--- api/index.js
+++ api/index.js
@@ -2,30 +2,17 @@ const express = require('express') // require -> commonJS
 const crypto = require('node:crypto')
 const cors = require('cors')
 
-const movies = require('../../movies.json')
-const { validateMovie, validatePartialMovie } = require('../../schemas/movies')
+const movies = require('../movies.json')
+const { validateMovie, validatePartialMovie } = require('../schemas/movies')
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-  origin: (origin, callback) => {
-    const ACCEPTED_ORIGINS = [
-      'http://localhost:8080',
-      'http://localhost:1234',
-      'https://movies.com'
-    ]
+const corsOptions = {
+  origin: ['http://localhost:8080', 'http://localhost:1234', 'https://movies.com'],
+  optionsSuccessStatus: 200
+};
 
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
-    }
-
-    if (!origin) {
-      return callback(null, true)
-    }
-
-    return callback(new Error('Not allowed by CORS'))
-  }
-}))
+app.use(cors(corsOptions));
 app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
 
 // métodos normales: GET/HEAD/POST
